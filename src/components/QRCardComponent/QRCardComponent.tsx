79878f05df7589e2c96890c6a036aba231fa07f6
Ben Angel <ben.angel@sendpayments.com>
2023-05-30 21:03:58 +1000

    initial commit

---
+++ src/components/QRCardComponent/QRCardComponent.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import React from 'react';
+import './qrCode.css';
+
+const QRCardComponent = () => {
+  return (
+    <Flex
+      flexDir='column'
+      justifyContent='space-between'
+      bgColor='hsl(0, 0%, 100%)'
+      borderRadius='20px'
+      // boxShadow='0px 0px 14px 5px rgba(0,0,0,0.16)'
+      width='300px'
+      height='470px'
+      position='relative'
+      p='16px'
+    >
+      <Image
+        alt='qr code'
+        height='576'
+        width='576'
+        sizes='(min-width: 60em) 24vw,(min-width: 28em) 45vw,100vw'
+        src={'/image-qr-code.png'}
+        // position='inherit'
+        style={{
+          borderRadius: '10px',
+          objectFit: 'contain',
+          height: 'auto',
+        }}
+      />
+      <Box lineHeight='initial' padding='0px 14px 14px 14px' textAlign='center'>
+        <Text fontSize='21px' fontWeight={700} pb={2}>
+          Improve your front-end skills by building projects
+        </Text>
+        <Text textColor='#757575' fontSize='15px' fontWeight={400} pt={'8px'}>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
+
+export default QRCardComponent;
