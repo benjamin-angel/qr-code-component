6b0e3650afb6aa7feb8835e6f7a8b274ad3a932f
Ben Angel <ben.angel@sendpayments.com>
2023-05-31 09:44:21 +1000

    add optimizeFonts to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/qr-code-component/' : '',
   images: {
     unoptimized: true,
   },
+  optimizeFonts: false,
 };
 
 module.exports = nextConfig;
